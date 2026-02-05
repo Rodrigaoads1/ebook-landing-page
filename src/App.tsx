91b7c582213966f269ed60a4dcdf7e8e7d29f905
Rodrigo <rodrigo@email.com>
2026-02-05 18:08:24 -0300

    Show ebook page as main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { EbookPage } from './pages/EbookPage';
+import { Hero } from './components/Hero';
+import { SocialProof } from './components/SocialProof';
+import { ContentSection } from './components/ContentSection';
+import { FinalCTA } from './components/FinalCTA';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/ebook" element={<EbookPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen bg-[#0a0a0a] text-white selection:bg-[#D4AF37]/30 selection:text-[#D4AF37]">
+      <main>
+        <Hero />
+        <SocialProof />
+        <ContentSection />
+        <FinalCTA />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
