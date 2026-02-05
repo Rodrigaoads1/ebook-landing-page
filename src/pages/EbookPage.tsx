665ca2f666d1e6230ebbf75dc8ae7af44f2e6b9a
Rodrigo <rodrigo@email.com>
2026-02-05 18:57:03 -0300

    Remove testimonials section

--- src/pages/EbookPage.tsx
+++ src/pages/EbookPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Hero } from '../components/Hero';
-import { SocialProof } from '../components/SocialProof';
 import { ContentSection } from '../components/ContentSection';
 import { FinalCTA } from '../components/FinalCTA';
 import { Footer } from '../components/Footer';
@@ -16,7 +15,6 @@ export function EbookPage() {
         <div className="min-h-screen bg-[#0a0a0a] text-white selection:bg-[#D4AF37]/30 selection:text-[#D4AF37]">
             <main>
                 <Hero onOpenModal={openModal} />
-                <SocialProof />
                 <ContentSection />
                 <FinalCTA onOpenModal={openModal} />
             </main>
